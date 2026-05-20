9a96736872ec71cfb0bf2c91bc02b90868fdc67f
Leonardo <leonardolatorre2000@gmail.com>
2026-05-20 19:01:26 +0200

    fix: adjust layout of GroupMemberCard component for better alignment of position and username

--- src/components/groupMemberCard.jsx
+++ src/components/groupMemberCard.jsx
@@ -2,7 +2,8 @@ const GroupMemberCard = (props) => {
   return (
     <>
       <div class="widget flex justify-between">
-        <div class="w-2/3 truncate">{props.member.username}</div>
+        <div class="w-1/4 text-start">{props.pos}</div>
+        <div class="w-2/4 truncate">{props.member.username}</div>
         <div class="w-1/3 text-end">{props.points}</div>
       </div>
     </>
